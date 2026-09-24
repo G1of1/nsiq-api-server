98462daab7e90402548f5fa890abe2de58265f70
G1of1 <ngsuna24@gmail.com>
2026-09-24 12:09:44 -0400

    removed old service logic for virus scanner

--- src/routes/virus.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-from src.services import virus
-
-router = APIRouter()
-
-@router.post("/virus-scan")
-async def scan_file(file: UploadFile = File(...)):
-    return await virus.virus_scan(file)
-
